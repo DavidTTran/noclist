03ea25cd4c8a40e2c7b2e4e351cede41828a18e8
David Tran <davidttemail@gmail.com>
2020-08-07 19:29:17 -0600

    Output user ids

---
+++ noclist.rb
@@ -0,0 +1,40 @@
+require 'Faraday'
+require 'digest'
+
+class Noclist
+  def initialize
+    @token = get_token
+    @checksum = get_checksum
+    @ids = get_user_ids
+  end
+
+  def display_ids
+    p @ids if @ids
+  end
+
+  private
+
+  def connection(path)
+    Faraday.new("http://localhost:8888/#{path}")
+  end
+
+  def get_token(attempts = 0)
+    return nil if attempts == 3
+    response = connection("auth").get
+    return response.headers["badsec-authentication-token"] if response.status == 200
+    get_token(attempts += 1)
+  end
+
+  def get_user_ids(attempts = 0)
+    return nil if attempts == 3 || @checksum == nil
+    response = connection("users").get do |request|
+      request.headers["X-Request-Checksum"] = @checksum
+    end
+    return response.body.split("\n") if response.status == 200
+    get_user_ids(attempts += 1)
+  end
+
+  def get_checksum
+    @token ? Digest::SHA2.hexdigest("#{@token}/users") : nil
+  end
+end
---
+++ noclist_runner.rb
@@ -0,0 +1,3 @@
+require_relative "noclist"
+
+Noclist.new.display_ids
